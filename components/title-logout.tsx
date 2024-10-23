a0f91c1f9afd138834a817c46e976a306c50d163
Kirill171 <kiria12312@mail.ru>
2024-10-23 21:32:44 +0300

    fix: fix logout button

--- components/title-logout.tsx
+++ components/title-logout.tsx
@@ -1,16 +1,18 @@
 import { useAuth } from '@/app/auth/AuthProvider';
-import { TouchableOpacity, Image } from 'react-native';
-import { ImageSourcePropType } from 'react-native';
+import { TouchableOpacity, Image, ImageSourcePropType } from 'react-native';
+import { useRouter } from 'expo-router';
 
 
 export default function TitleLogout() {
   const { user, logout } = useAuth();
+  const router = useRouter();
 
   const exitIcon: ImageSourcePropType = require('@/assets/images/exit.png');
 
   const handleLogout = async () => {
     if (user) {
       await logout();
+      router.push('/');
     } else {
       console.error('Пользователь не авторизован');
     }
